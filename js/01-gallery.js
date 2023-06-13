e95391e01960311a6c71d0c6ae3ccebf43b56f44
PetroKasperskyi <kaspersky28061993@gmail.com>
2023-06-13 18:40:35 +0300

    finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,32 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryContainerEl = document.querySelector('.gallery');
-const markup = createGalleryItemsMurkup(galleryItems);
-galleryContainerEl.insertAdjacentHTML('beforeend', markup)
-
-
-function createGalleryItemsMurkup(items) {
-    return items.map(({ preview, original, description }) => {
-        return `<li class="gallery__item">
+const markup = galleryItems.map(({ preview, original, description }) => 
+    `<li class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img class="gallery__image"
     src="${preview}" 
     data-source="${original}" 
-    alt="${description}" >
+    alt="${description}" />
   </a>
 </li>`
-}).join('');
-}
-galleryContainerEl.addEventListener('clck', onClick)
-function onClick(evt) {
-    
-    if (evt.target.nodeName !== "BUTTON") {
-        return;  
-    }
-    console.dir(evt.target);
-}
-//  const instance = basicLightbox.create(`<img src="${evt.dataset.source}" width="800" height="600">`);
-// instance.show()
+  );
 
+galleryContainerEl.insertAdjacentHTML('beforeend', markup.join(''))
+galleryContainerEl.addEventListener('click', onClick)
+function onClick(event) {
+  event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+      return; 
+    }
+
+
+const instance = basicLightbox.create(`
+ <img src="${event.target.dataset.source}" width="800" height="600">`);
+  instance.show()  
+}
 
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,23 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const listEl = document.querySelector('.gallery');
+const itemsel = createGalleryItems(galleryItems);
+listEl.insertAdjacentHTML('beforeend', itemsel)
+
+function createGalleryItems(items) {
+    return items.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img  class="gallery__image" src="${preview}" alt="${description}">
+  </a>
+</li>`;
+    }).join("");
+}
+    let lightbox = new SimpleLightbox(".gallery a", {
+        captions: true,
+        captionsData: "alt",
+        captionsDelay: 250,
+        
+    })
